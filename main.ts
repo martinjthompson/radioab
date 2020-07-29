43eb7e158c1285280f82957d8d1e865a1c920e43
Martin Thompson <martin@parallelpoints.com>
2020-07-29 11:48:47 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,21 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
-    led.plot(0, 0)
-})
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(3)
-    led.plot(0, 1)
-    led.plot(0, 0)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(2)
-    led.plot(0, 1)
-})
 radio.setGroup(127)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
-    	
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+        led.plot(0, 0)
+        led.unplot(0, 1)
+    } else if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+        led.unplot(0, 0)
+        led.plot(0, 1)
+    } else if (input.buttonIsPressed(Button.AB)) {
+        radio.sendNumber(3)
+        led.plot(0, 1)
+        led.plot(0, 0)
     } else {
+        radio.sendNumber(0)
         led.unplot(0, 0)
         led.unplot(0, 1)
     }
+    basic.pause(100)
 })
