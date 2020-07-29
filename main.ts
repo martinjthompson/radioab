82c75222a3ff607e52980c5898d2a15005f24fe6
Martin Thompson <martin@parallelpoints.com>
2020-07-29 11:51:57 +0100

    buttons read in correct priority

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 radio.setGroup(127)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        radio.sendNumber(1)
+    if (input.buttonIsPressed(Button.AB)) {
+        radio.sendNumber(3)
         led.plot(0, 0)
         led.unplot(0, 1)
     } else if (input.buttonIsPressed(Button.B)) {
         radio.sendNumber(2)
         led.unplot(0, 0)
         led.plot(0, 1)
-    } else if (input.buttonIsPressed(Button.AB)) {
-        radio.sendNumber(3)
-        led.plot(0, 1)
+    } else if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
         led.plot(0, 0)
+        led.unplot(0, 1)
     } else {
         radio.sendNumber(0)
         led.unplot(0, 0)
